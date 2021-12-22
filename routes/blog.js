0646491d385cb3e66d6b7dbac633637c0fa5798b
Kristaps Ritins <kritinsdev@gmail.com>
2021-12-22 23:46:15 +0200

    single post details

--- routes/blog.js
+++ routes/blog.js
@@ -34,9 +34,28 @@ router.post('/posts', async (req, res) => {
         req.body.summary,
         req.body.content,
         req.body.author
-    ]
+    ];
+
     await db.query('INSERT INTO posts (title, summary, body, author_id) VALUES (?)', [data]);
     res.redirect('/posts');
+});
+
+router.get('/posts/:id', async (req, res) => {
+    const query = `
+    SELECT posts.*, authors.name as author_name, authors.email as author_email FROM posts 
+    INNER JOIN authors ON posts.author_id = authors.id
+    WHERE posts.id = ?
+    `;
+
+    const [postInfo] = await db.query(query, [req.params.id]);
+
+    if (!postInfo || postInfo.length === 0) {
+        return res.status(404).render('404');
+    }
+
+    res.render('post-detail', {
+        post: postInfo[0]
+    });
 });
 
 module.exports = router;
