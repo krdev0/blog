609b449d98fc3f6b33ad84aa145a567ff8e18bb8
Kristaps Ritins <kritinsdev@gmail.com>
2022-06-09 23:26:40 +0300

    custom auth

---
+++ resources/views/register/create.blade.php
@@ -0,0 +1,9 @@
+<x-layout>
+    <section class="px-6 py-8">
+        <main class="max-w-lg">
+            <form action="/register" method="POST">
+
+            </form>
+        </main>
+    </section>
+</x-layout>
