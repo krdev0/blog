3bba4fa9bce5aaa406bda81be9d572b1efaf77ac
Kristaps Ritins <kritinsdev@gmail.com>
2022-06-11 13:58:00 +0300

    login functionality

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -3,12 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class SessionsController extends Controller
 {
     public function create()
     {
         return view('sessions.create');
+    }
+
+    public function store()
+    {
+        //validate request
+        $attributes = request()->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        //attempt to auth and login user based on provided credentials
+        if(auth()->attempt($attributes)) {
+            session()->regenerate();
+            
+            return redirect('/')->with('success', 'Welcome Back!');
+        }
+
+        //auth failed
+
+        //option 1
+//        return back()
+//            ->withInput()
+//            ->withErrors(['email' => 'Your provided credentials could not be verified']);
+
+        //option 2
+        throw ValidationException::withMessages([
+            'email' => 'Your provided credentials could not be verified'
+        ]);
     }
     
     public function destroy()
--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -1,9 +1,43 @@
 <x-layout>
     <section class="px-6 py-8">
         <main class="max-w-lg mx-auto">
-            <form action="/register" method="POST" class="p-6 bg-gray-100 rounded-xl">
+            <form action="/login" method="POST" class="p-6 bg-gray-100 rounded-xl">
                 @csrf
                 <h1 class="font-bold text-3xl mb-8 text-center">Log In</h1>
+
+                <div class="mb-6">
+                    <label for="email" class="block mb-2 uppercase font-bold text-xs text-gray-700">
+                        Email
+                    </label>
+
+                    <input type="email" class="border border-gray-200 rounded-xl p-2 w-full"
+                           name="email"
+                           id="email"
+                           value="{{old('email')}}"
+                           required
+                    >
+
+                    @error('email')
+                    <p class="text-red-500 text-xs mt-2">{{$message}}</p>
+                    @enderror
+                </div>
+                <div class="mb-6">
+                    <label for="password" class="block mb-2 uppercase font-bold text-xs text-gray-700">
+                        Password
+                    </label>
+
+                    <input type="password" class="border border-gray-200 rounded-xl p-2 w-full"
+                           name="password"
+                           id="password"
+                           required
+                    >
+                </div>
+
+                <div class="mb-6">
+                    <button type="submit" class="px-4 py-2 bg-blue-500 rounded-full text-white">
+                        Submit
+                    </button>
+                </div>
             </form>
         </main>
     </section>
